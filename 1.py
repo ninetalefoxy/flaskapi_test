4cfaaebee4c6d51f9f0c2fb8087e210f79771fb7
jagtap <suraj100jagtap@gmail.com>
2021-08-27 11:16:13 +0530

    changed

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 
 
-app = Flask(__name__)
+app = Flask(__name__)  
 
 data = [{"name":"shubham"},                                                    
          {"name":"akshay"},                                                    
